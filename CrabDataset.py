6e3dfd04e12810fa19ae591f9a8e5055750999fc
WTFPUn <punn_namw@hotmail.com>
2024-04-16 23:35:36 +0700

    add Dataset class and VAE class(will place in otther repo later)

---
+++ CrabDataset.py
@@ -0,0 +1,20 @@
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+
+
+class CrabDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = os.listdir(root_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.images[idx])
+        image = Image.open(img_name)
+        if self.transform:
+            image = self.transform(image)
+        return image
